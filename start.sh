88b3bdc2e5d7d195faed145c8295829178152f24
Endrick <ehpb2014@gmail.com>
2025-11-02 12:12:12 -0300

    a

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-echo "Iniciando servidor CS 1.6 + Playit..."
+echo "Iniciando CS 1.6 + Playit..."
 
-# Inicia o servidor em background
 cd /hlds/cstrike
+
+# Inicia o servidor CS 1.6 em background
 screen -dmS cs16 ./hlds_run -game cstrike -console -port 27015 -maxplayers 16 +map de_dust2
 
-# Espera um pouco pro HLDS subir
+# Espera o HLDS subir
 sleep 10
 
-# Inicia o túnel Playit (substitua pelo seu token)
-playit --token $PLAYIT_TOKEN
+# Faz login no Playit com o token
+playit login --token "$PLAYIT_TOKEN"
+
+# Inicia o túnel (use o nome que você criou no painel)
+playit tunnel udp --name cs16 --port 27015
